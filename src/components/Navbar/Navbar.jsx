a29e3d66a19c40eed24cb1bcf6b007d3aa859469
AGonzL <agonzlopez.11@gmail.com>
2024-05-20 23:53:53 +0200

    feat: add Header and Subheading components
    - Created Header component with site logo and navigation links
    - Added Subheading component for section titles
    - Applied basic styling to Header and Subheading components
    - Integrated Header into the main layout
    - Used Subheading in the Menu section to introduce categories
    -navbar style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         {toggleMenu && (
           <div className="app__navbar-smallscreen_overlay flex_center slide-bottom">
             <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
-            <ul className="app__navbar-smallscreen-links">
+            <ul className="app__navbar-smallscreen_links">
               <li className="p__opensans"><a href="#home">Home</a></li>
               <li className="p__opensans"><a href="#home">About</a></li>
               <li className="p__opensans"><a href="#home">Menu</a></li>
