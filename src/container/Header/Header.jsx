6ac31a0aae8d236f848bc96793ff70ef6c49d0b9
AGonzL <agonzlopez.11@gmail.com>
2024-05-22 01:26:29 +0200

    feat: Update Footer and Header components
    - Modified src/components/Footer/FooterOverlay.css to enhance styles for better responsiveness.
    - Updated src/components/Footer/FooterOverlay.jsx to include new overlay functionalities.
    - Improved styles in src/components/Footer/Newsletter.css for a more modern look.
    - Refactored src/components/Footer/Newsletter.jsx to streamline newsletter subscription logic.
    - Enhanced src/container/Footer/Footer.css with additional styling adjustments.
    - Refactored src/container/Footer/Footer.jsx to improve component structure and readability.
    - Updated src/container/Header/Header.css to align with new design guidelines.
    - Modified src/container/Header/Header.jsx to incorporate new header functionalities and improve performance.
    
    These changes aim to improve the overall user experience and maintain code quality and scalability.

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
       <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-    <button type="button" className="custom__buttom">Explore Menu</button>
+    <button type="button" className="custom__button">Explore Menu</button>
     </div>
     <div className="app__wrapper_img">
       <img src={images.welcome} alt="header img" />
