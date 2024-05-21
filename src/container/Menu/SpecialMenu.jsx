981b07a0a4fd77b7b3ec7d36fba4d57eaa393107
AGonzL <agonzlopez.11@gmail.com>
2024-05-21 02:00:03 +0200

    implement menu item and css styles

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -17,7 +17,7 @@ const SpecialMenu = () => (
         <p className="app_specialMenu_menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
         {data.wines.map((wine, index) => (
-          <p>{wine.title}</p>
+          <MenuItem key={wine.title + index} title={wine.title} price={wine.price}tags={wine.tags}/>
         ))}
         </div>
       </div>
@@ -31,7 +31,7 @@ const SpecialMenu = () => (
         <p className="app_specialMenu_menu_heading">Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
         {data.cocktails.map((cocktail, index) => (
-          <p>{cocktail.title}</p>
+          <MenuItem key={cocktail.title + index} title={cocktail.price} tags={cocktail.tags}/>
         ))}
         </div>
       </div>
